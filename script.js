3ae4b72e91bc4fe838ca95a8dcd9448cfc5ffa78
Justin Xia <justinxia9@gmail.com>
2021-11-04 10:56:18 -0400

    Add heading and button to switch images

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
+const button = document.querySelector(".new-image");
+
 var request = new XMLHttpRequest();
 
 const image = document.getElementById("randomCat")
-const container = document.createElement('div');
-container.setAttribute('class', 'container');
 
 request.open('GET', 'https://api.thecatapi.com/v1/images/search', true);
 
@@ -10,5 +10,10 @@ request.onload = function() {
   var data = JSON.parse(this.response);
   image.src = data[0].url;
 }
+
+
+button.addEventListener("click", function() {
+  location.reload();
+});
 
 request.send();
