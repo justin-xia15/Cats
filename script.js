00a9d6d0f05d2646e555b5bdbee1c00a6bd23581
Justin Xia <justinxia9@gmail.com>
2021-11-07 11:49:48 -0500

    Change endpoint

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ const temperament = document.querySelector(".temperament")
 
 var request = new XMLHttpRequest();
 
-const image = document.getElementById("randomCat")
+const image = document.getElementById("cat")
 
 var settings = {
   "async": true,
   "crossDomain": true,
-  "url": "https://api.thecatapi.com/v1/breeds/search?q=sib",
+  "url": "https://api.thecatapi.com/v1/images/search?breed_ids=beng",
   "method": "GET",
   "headers": {
     "dd13c657-0fe0-4464-a2c3-d740b0ca1456": "DEMO-API-KEY"
@@ -23,29 +23,5 @@ var settings = {
 
 $.ajax(settings).done(function (response) {
   var data = response[0];
-  name.innerText = `${data.name}`;
-  origin.innerText = `Origin: ${data.origin}`;
-  description.innerText = `Description: ${data.description}`;
-  temperament.innerText = `Temperament: ${data.temperament}`;
-
   console.log(data);
 });
-
-
-// request.open('GET', 'https://api.thecatapi.com/v1/images/search', true);
-// request.open('GET', 'https://api.thecatapi.com/v1/breeds/search?q=sib?api_key=dd13c657-0fe0-4464-a2c3-d740b0ca1456', true);
-//
-//
-// request.onload = function() {
-//   var data = JSON.parse(this.response);
-//   console.log(data);
-//   var data = JSON.parse(this.response);
-//   image.src = data[0].url;
-// }
-//
-//
-// button.addEventListener("click", function() {
-//   location.reload();
-// });
-//
-// request.send();
