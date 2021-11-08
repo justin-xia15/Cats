8ad5602ba77aa4ee3fe08e8398463be7e9ca6220
Justin Xia <justinxia9@gmail.com>
2021-11-08 12:43:54 -0500

    Add cat info

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const image = document.getElementById("cat")
 var settings = {
   "async": true,
   "crossDomain": true,
-  "url": "https://api.thecatapi.com/v1/images/search?breed_ids=beng",
+  "url": "https://api.thecatapi.com/v1/images/search?breed_ids=asho",
   "method": "GET",
   "headers": {
     "dd13c657-0fe0-4464-a2c3-d740b0ca1456": "DEMO-API-KEY"
@@ -23,7 +23,12 @@ var settings = {
 
 $.ajax(settings).done(function (response) {
   var data = response[0];
+  var info = data.breeds[0];
   image.src = data.url;
-  console.log(data.breeds[0]);
+  console.log(info);
+  name.innerText = info.name;
+  origin.innerText = `Origin: ${info.origin}`;
+  description.innerText = `Description: ${info.description}`;
+  temperament.innerText = `Temperament: ${info.temperament}`;
 
 });
